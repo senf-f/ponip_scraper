51203181e8c8a97304a09684da0fd8e9682dd506
mate.mrse <mate.mrse@gmail.com>
2024-12-28 12:57:06 +0100

    Refactoring: dodano pisanje u bazu

--- data.py
+++ data.py
@@ -1,30 +1,37 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Optional
 
+from sqlalchemy import Column, Integer, String, Float, DateTime, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+# Base class for all SQLAlchemy models
+Base = declarative_base()
+
+
+# Nekretnina model mapped to the properties table
 @dataclass
-class Nekretnina:
-    nadlezno_tijelo: str
-    poslovni_broj: str
-    opis: str
-    vrsta_predmeta: str
-    opseg_imovine: str
-    utvrdjena_vrijednost: float
-    napomena_uz_detalje: str
-    id_nadmetanja: int
-    broj_drazbe: str
-    datum_odluke: datetime
-    datum_pocetka: datetime
-    datum_pocetka_nadmetanja: datetime
-    datum_zavrsetka_nadmetanja: datetime
-    ostali_uvjeti_prodaje: Optional[str]
-    min_cijena: float
-    pocetna_cijena: float
-    iznos_drazbenog_koraka: float
-    jamcevina: float
-    ostali_uvjeti_za_jamcevinu: Optional[str]
-    razgledavanje: Optional[str]
-    napomena_uz_uvjete_prodaje: Optional[str]
-    iznos_najvise_ponude: Optional[str] = None
-    status_nadmetanja: Optional[str] = None
+class Nekretnina(Base):
+    __tablename__ = "properties"  # Specifies the database table name
 
+    # Column definitions
+    id_nadmetanja: int = Column(Integer, primary_key=True)  # Primary key, scraped from data
+    nadlezno_tijelo: str = Column(Text, nullable=False)
+    poslovni_broj: str = Column(String, nullable=False)
+    opis: str = Column(Text, nullable=False)
+    vrsta_predmeta: str = Column(Text, nullable=False)
+    opseg_imovine: str = Column(Text, nullable=False)
+    utvrdjena_vrijednost: float = Column(Float, nullable=False)
+    napomena_uz_detalje: str = Column(Text, nullable=True)
+    broj_drazbe: str = Column(String, nullable=False)
+    datum_odluke: datetime = Column(DateTime, nullable=False)
+    datum_pocetka: datetime = Column(DateTime, nullable=False)
+    datum_pocetka_nadmetanja: datetime = Column(DateTime, nullable=False)
+    datum_zavrsetka_nadmetanja: datetime = Column(DateTime, nullable=False)
+    ostali_uvjeti_prodaje: str = Column(Text, nullable=True)
+    min_cijena: float = Column(Float, nullable=False)
+    pocetna_cijena: float = Column(Float, nullable=False)
+    iznos_drazbenog_koraka: float = Column(Float, nullable=False)
+    jamcevina: float = Column(Float, nullable=False)
+    ostali_uvjeti_za_jamcevinu: str = Column(Text, nullable=True)
+    razgledavanje: str = Column(Text, nullable=True)
+    napomena_uz_uvjete_prodaje: str = Column(Text, nullable=True)
--- lean.py
+++ lean.py
@@ -5,12 +5,29 @@ from datetime import datetime
 from time import perf_counter
 
 import requests
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from configurator import load_config, validate_config
-from data import Nekretnina
+from data import Nekretnina, Base  # Updated for SQLAlchemy ORM integration
 
+# Load and validate configuration
 CONFIG = load_config()
 validate_config(CONFIG)
+
+load_dotenv()
+# Detect environment (default to development)
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+# Set database URL based on the environment
+if ENVIRONMENT == "production":
+    DATABASE_URL = os.getenv("DATABASE_URL")
+else:
+    # DATABASE_URL=postgresql://ponipuser:ponipuser1337@localhost:5432/real_estate_db
+    DATABASE_URL = f"sqlite:///{os.getcwd()}/test_database.db"
+# Database configuration
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 BASE_DIR = CONFIG["directory_base"]
 LOG_FILES = CONFIG["log_files"]
@@ -24,8 +41,8 @@ VRSTA_NEKRETNINE = CONFIG["vrsta_nekretnine"]
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[logging.FileHandler(os.path.join(BASE_DIR, log_file).replace("\\", "/")) for log_file in LOG_FILES] + [
-        logging.StreamHandler()]
+    handlers=[logging.FileHandler(os.path.join(BASE_DIR, log_file).replace("\\", "/")) for log_file in LOG_FILES] +
+             [logging.StreamHandler()]
 )
 
 
@@ -33,7 +50,6 @@ def parsiraj_csv():
     start = perf_counter()
     errors = ""
     nepotpuni = []
-    data_items = []
     logging.info(f"[*] Current directory: {os.getcwd()}")
 
     # Download the CSV file
@@ -45,107 +61,106 @@ def parsiraj_csv():
     with open(os.path.join(BASE_DIR, 'idevi')) as f:
         id_evi = f.read().splitlines()
 
-    # Open the CSV file
-    with open(csv_path, 'r', encoding='utf-8') as f:
+    # Open a database session
+    session = SessionLocal()
 
-        # Skip first line (header)
-        next(f)
-        counter = 0
-        error_counter = 0
+    try:
+        # Open the CSV file
+        with open(csv_path, 'r', encoding='utf-8') as f:
+            # Skip first line (header)
+            next(f)
+            counter = 0
+            error_counter = 0
 
-        # Iterate over the lines
-        for count, line in enumerate(f):
-            # Split the line on the comma separator
-            fields = line.strip().split(';')
+            # Iterate over the lines
+            for count, line in enumerate(f):
+                # Split the line on the comma separator
+                fields = line.strip().split(';')
 
-            try:
-                # Nepotpuni podaci
-                if len(fields) != 26 or fields[13] == "":
-                    nepotpuni += [fields]
-                    ## MM kako hendlati nepotpune
+                try:
+                    # Nepotpuni podaci
+                    if len(fields) != 26 or fields[13] == "":
+                        nepotpuni += [fields]
+                        ## MM kako hendlati nepotpune
+                        continue
+
+                    # Datum i vrijeme početka nadmetanja
+                    if datetime.strptime(fields[13][1:-1], "%Y-%m-%d %H:%M:%S") < datetime.today():
+                        continue
+
+                    # Je li već korišten
+                    if fields[8] in id_evi:
+                        continue
+
+                    # Opis
+                    if not any(grad in fields[2][1:-1] for grad in GRADOVI):
+                        continue
+
+                    if not any(vrsta in fields[2][1:-1] for vrsta in VRSTA_NEKRETNINE):
+                        continue
+
+                    # Minimalna zakonska cijena ispod koje se predmet prodaje ne može prodati
+                    if float(fields[16][1:-1]) > MAX_PRICE:
+                        continue
+
+                    counter += 1
+
+                except ValueError as err:
+                    # Debug dio zbog neujednačenog formatiranja
+                    errors += f"\nID: {fields[8]} | Error: {err}"
+                    error_counter += 1
                     continue
 
-                # Datum i vrijeme početka nadmetanja
-                if datetime.strptime(fields[13][1:-1], "%Y-%m-%d %H:%M:%S") < datetime.today():
-                    continue
+                # Create a Nekretnina instance
+                item = Nekretnina(
+                    nadlezno_tijelo=fields[0],
+                    poslovni_broj=fields[1],
+                    opis=fields[2][1:-1],
+                    vrsta_predmeta=fields[3],
+                    opseg_imovine=fields[4],
+                    utvrdjena_vrijednost=float(fields[5]),
+                    napomena_uz_detalje=fields[6],
+                    id_nadmetanja=int(fields[8]),
+                    broj_drazbe=fields[9],
+                    datum_odluke=datetime.strptime(fields[10][1:-1], "%Y-%m-%d"),
+                    datum_pocetka=datetime.strptime(fields[11][1:-1], "%Y-%m-%d %H:%M:%S"),
+                    datum_pocetka_nadmetanja=datetime.strptime(fields[12][1:-1], "%Y-%m-%d %H:%M:%S"),
+                    datum_zavrsetka_nadmetanja=datetime.strptime(fields[13][1:-1], "%Y-%m-%d %H:%M:%S"),
+                    ostali_uvjeti_prodaje=fields[15],
+                    min_cijena=float(fields[16][1:-1]),
+                    pocetna_cijena=float(fields[17]),
+                    iznos_drazbenog_koraka=float(fields[18]),
+                    jamcevina=float(fields[20]),
+                    ostali_uvjeti_za_jamcevinu=fields[21],
+                    razgledavanje=fields[22],
+                    napomena_uz_uvjete_prodaje=fields[23]
+                )
 
-                # Je li već korišten
-                if fields[8] in id_evi:
-                    continue
+                # Add to database
+                session.merge(item)
+                id_evi.append(fields[8])
 
-                # Opis
-                if not any(grad in fields[2][1:-1] for grad in GRADOVI):
-                    continue
+                # Notify and log the result
+                result = f"ID: {item.id_nadmetanja} | Opis: {item.opis} | Cijena: {item.pocetna_cijena}"
+                send_to_telegram(content=f"Novo na PONIP scraperu:\n{result}")
+                logging.info(result)
 
-                if not any(vrsta in fields[2][1:-1] for vrsta in VRSTA_NEKRETNINE):
-                    continue
-
-                # Minimalna zakonska cijena ispod koje se predmet prodaje ne može prodati
-                if float(fields[16][1:-1]) > MAX_PRICE:
-                    continue
-
-                counter += 1
-
-            except ValueError as err:
-                # Debug dio zbog neujednačenog formatiranja
-                errors += f"\nID: {fields[8]} | Error: {err}"
-                error_counter += 1
-
-            # Process the line
-            result = "---------------------------------------"
-            result += f"\nID: {fields[8]} | Početna cijena: {fields[17]} | Oznaka EJD: {fields[9]} | Poslovni broj spisa: {fields[1]}"
-            result += "\n---------------------------------------"
-            result += f"\nOpis: {fields[2]}"
-            result += f"\nNapomena: {fields[6]}"
-            result += f"\nDatum početka i završetka nadmetanja: {fields[12]} - {fields[13]}"
-            result += f"\nRazgledavanje: {fields[22]}"
-            result += f"\nDodatna napomena: {fields[23]}"
-            id_evi.append(fields[8])
-
-            # Create a RealEstateItem instance
-            item = Nekretnina(
-                nadlezno_tijelo=fields[0],
-                poslovni_broj=fields[1],
-                opis=fields[2][1:-1],
-                vrsta_predmeta=fields[3],
-                opseg_imovine=fields[4],
-                utvrdjena_vrijednost=float(fields[5]),
-                napomena_uz_detalje=fields[6],
-                id_nadmetanja=int(fields[8]),
-                broj_drazbe=fields[9],
-                datum_odluke=datetime.strptime(fields[10][1:-1], "%Y-%m-%d"),
-                datum_pocetka=datetime.strptime(fields[11][1:-1], "%Y-%m-%d %H:%M:%S"),
-                datum_pocetka_nadmetanja=datetime.strptime(fields[12][1:-1], "%Y-%m-%d %H:%M:%S"),
-                datum_zavrsetka_nadmetanja=datetime.strptime(fields[13][1:-1], "%Y-%m-%d %H:%M:%S"),
-                ostali_uvjeti_prodaje=fields[15],
-                min_cijena=float(fields[16][1:-1]),
-                pocetna_cijena=float(fields[17]),
-                iznos_drazbenog_koraka=float(fields[18]),
-                jamcevina=float(fields[20]),
-                ostali_uvjeti_za_jamcevinu=fields[21],
-                razgledavanje=fields[22],
-                napomena_uz_uvjete_prodaje=fields[23]
-            )
-
-            # Add to list for further processing
-            data_items.append(item)
-
-            send_to_telegram(content=f"Novo na PONIP scraperu:\n{result}")
-            logging.info(f"{result=}")
+            # Commit all changes to the database
+            session.commit()
 
         footer = f"Counter: {counter} | Errors: {error_counter} | Performance: {perf_counter() - start} s\n"
         footer += "https://ponip.fina.hr/ocevidnik-web/pretrazivanje/nekretnina"
+        logging.info(footer)
 
-        logging.info(str(datetime.now()) + " " + footer.split('\n')[0])
-
+        # Update processed IDs
         with open(f"{BASE_DIR}idevi", mode="wt") as fi:
             fi.write("\n".join(id_evi))
 
         if error_counter:
             send_to_telegram(content=f"Greške na 'PONIP' scraperu:\n{errors}\n{footer}")
 
-        if counter:
-            send_to_telegram(content=footer)
+    finally:
+        session.close()
 
 
 def send_to_telegram(content):
@@ -157,12 +172,19 @@ def send_to_telegram(content):
     api_url = f"https://api.telegram.org/bot{api_token}/sendMessage"
 
     try:
-        response = requests.post(api_url, json={'chat_id': chat_id, 'text': content}, timeout=10)
-        response.raise_for_status()
+        # response = requests.post(api_url, json={'chat_id': chat_id, 'text': content}, timeout=10)
+        # response.raise_for_status()
         logging.info("Telegram message sent successfully.")
     except requests.RequestException as e:
         logging.error(f"Failed to send Telegram message: {e}")
 
 
+def initialize_database():
+    logging.info("Initializing database...")
+    Base.metadata.create_all(engine)
+    logging.info("Database initialized.")
+
+
 if __name__ == '__main__':
+    initialize_database()
     parsiraj_csv()
